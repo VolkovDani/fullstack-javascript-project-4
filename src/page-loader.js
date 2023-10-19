37ca0b3d411fa80c890c8099ad4d1e5c928c84df
Danil Volkov <volchara287@gmail.com>
2023-10-19 17:10:01 +0800

    Вынесение функция для скачивания изображений

--- src/page-loader.js
+++ src/page-loader.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { access, writeFile, mkdir } from 'fs/promises';
-import * as cheerio from 'cheerio';
+import { access, writeFile } from 'fs/promises';
 import { StringMaker } from './utils/pathsAndStrings.js';
+import downloadImages from './siteObjects/image.js';
 
 const pageloader = (strToSite, pathToSave = '/home/user/<current-dir>') => new Promise((resolve, reject) => {
   let pathToSaveFile = pathToSave;
@@ -14,32 +14,8 @@ const pageloader = (strToSite, pathToSave = '/home/user/<current-dir>') => new P
   // Если файла нет(нет возможности узнать access), то продолжаем
     .catch(() => {
       axios.get(strToSite)
-        .then(({ data }) => {
-          const arrPromisesIMGs = [];
-          const $ = cheerio.load(data);
-          const arrImgs = $('img');
-          const pathForFolderWithIMGs = stringMaker.makePathForFolderWithIMGs();
-          arrImgs.each((i, el) => {
-            let srcCurrentElement;
-            if (el.attribs.src.search(/\/(?<=^.)/g) === -1) srcCurrentElement = el.attribs.src;
-            else srcCurrentElement = stringMaker.makeStrSiteWithoutDirs(el.attribs.src);
-            // Промис для создания файла с изображением
-            const makingFile = (dataImg) => {
-              const pathToImg = stringMaker.makePathToImg(srcCurrentElement);
-              return writeFile(pathToImg, dataImg);
-            };
-            // Создать промис по скачиванию и созданию файла изображения
-            // для каждого элемента и добавить в массив
-            // чтобы потом отправить в Promise.all
-            const downloadImage = axios.get(srcCurrentElement.startsWith('http') ? srcCurrentElement : 'https://'.concat(srcCurrentElement), { responseType: 'blob' })
-              .then((response) => makingFile(response.data));
-
-            arrPromisesIMGs.push(downloadImage);
-            el.attribs.src = stringMaker.makeURLtoFileAsset(srcCurrentElement);
-          });
-          return mkdir(pathForFolderWithIMGs).then(() => Promise.all(arrPromisesIMGs))
-            .then(() => writeFile(savePath, $.html()));
-        })
+        .then(({ data }) => downloadImages(data, stringMaker))
+        .then((htmlData) => writeFile(savePath, htmlData))
         .then(() => resolve(savePath))
         .catch((err) => reject(err));
     });
---
+++ src/siteObjects/image.js
@@ -0,0 +1,39 @@
+import * as cheerio from 'cheerio';
+import { writeFile, mkdir } from 'fs/promises';
+import axios from 'axios';
+
+const downloadImages = (data, stringMaker) => {
+  const arrPromisesIMGs = [];
+  const $ = cheerio.load(data);
+  const arrImgs = $('img');
+  const pathForFolderWithIMGs = stringMaker.makePathForFolderWithIMGs();
+  arrImgs.each((i, el) => {
+    /**
+     * Проверяет ссылку на ассет и в случае необходимости подставляет недостающие элементы ссылки
+     * @returns url для скачивания ассета
+     */
+    const getSrcCurrentElement = () => {
+      if (el.attribs.src.search(/\/(?<=^.)/g) === -1) return el.attribs.src;
+      return stringMaker.makeStrSiteWithoutDirs(el.attribs.src);
+    };
+    const srcCurrentElement = getSrcCurrentElement();
+    // Промис для создания файла с изображением
+    const makingFile = (dataImg) => {
+      const pathToImg = stringMaker.makePathToImg(srcCurrentElement);
+      return writeFile(pathToImg, dataImg);
+    };
+    // Создать промис по скачиванию и созданию файла изображения
+    // для каждого элемента и добавить в массив
+    // чтобы потом отправить в Promise.all
+    const downloadImage = axios.get(srcCurrentElement, { responseType: 'blob' })
+      .then((response) => makingFile(response.data));
+
+    arrPromisesIMGs.push(downloadImage);
+    el.attribs.src = stringMaker.makeURLtoFileAsset(srcCurrentElement);
+  });
+  return mkdir(pathForFolderWithIMGs)
+    .then(() => Promise.all(arrPromisesIMGs))
+    .then(() => $.html());
+};
+
+export default downloadImages;
