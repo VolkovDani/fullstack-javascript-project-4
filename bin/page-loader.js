06b7c9a611ef0d2386f090f73470f2ab325f4f2c
Danil Volkov <volchara287@gmail.com>
2024-01-15 08:41:04 +0400

    fix: commit for tests

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 import commanderConfig from '../src/cli.js';
 
-const myArgs = process.argv.filter((opt) => opt !== '--colors' && '--runInBand');
+const myArgs = process.argv.filter((opt) => {
+  const testParams = ['--colors', 'runInBand'];
+  return testParams.includes(opt);
+});
 commanderConfig.parse(myArgs, './src/page-loader.js');
