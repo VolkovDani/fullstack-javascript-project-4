28773a3a6a85e65aabe3a39c5be590a6f1ac810a
Danil Volkov <volchara287@gmail.com>
2024-01-15 16:42:55 +0400

    fix: add toString before writing data

--- src/siteObjects/links.js
+++ src/siteObjects/links.js
@@ -19,7 +19,7 @@ const downloadLinks = ($, stringMaker) => {
       let pathToFile;
       if (attribs.rel === 'canonical' || (attribs.rel === 'alternate' && attribs.type !== 'application/rss+xml')) pathToFile = stringMaker.makePathElementFile(hrefCurrentElement).concat('.html');
       else pathToFile = stringMaker.makePathElementFile(hrefCurrentElement);
-      return writeFile(pathToFile, dataHref);
+      return writeFile(pathToFile, dataHref.toString());
     };
     const downloadLink = axios.get(hrefCurrentElement, { responseType: 'document' })
       .then((response) => {
