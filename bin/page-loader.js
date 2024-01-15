b84d606a875ed7bded227613488aadff3608d0e7
Danil Volkov <volchara287@gmail.com>
2024-01-15 08:48:00 +0400

    fix: commit for test

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
 import commanderConfig from '../src/cli.js';
 
-const myFunc = () => {
-  console.log(process.argv);
-  return process.argv;
-};
-
-commanderConfig.parse(myFunc(), './src/page-loader.js');
+commanderConfig.parse();
