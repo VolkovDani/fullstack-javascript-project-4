b0e4427ad4285249981148a13ea1ef8f84277e24
Danil Volkov <volchara287@gmail.com>
2024-01-25 22:14:03 +0400

    fix: solve problem with paths

--- src/utils/versionCatcher.js
+++ src/utils/versionCatcher.js
@@ -1,5 +1,8 @@
 import { readFile } from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 // Function for getting version project from package.json
-const packageObj = await readFile(path.join(path.resolve(), './package.json'), 'utf-8');
+const packageObj = await readFile(path.join(dirname, '../../package.json'), 'utf-8');
 export default JSON.parse(packageObj).version;
