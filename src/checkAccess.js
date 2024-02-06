bfc805b453d377653a9a52bda761f220fc79263e
Danil Volkov <volchara287@gmail.com>
2024-02-06 16:19:03 +0400

    feat: add more loggers for better debugging

--- src/checkAccess.js
+++ src/checkAccess.js
@@ -1,11 +1,17 @@
 import { access } from 'fs/promises';
+import log from './utils/debugEl.js';
 
-export default (path) => access(path)
-  .then(() => {
-    const userErr = { code: 'EEXIST' };
-    throw userErr;
-  })
-  .catch((err) => {
-    if (err.code === 'ENOENT') return;
-    throw err;
-  });
+export default (path) => {
+  const logTool = log.extend('checkAccess');
+  return access(path)
+    .then(() => {
+      logTool('Check access: ', `${path}`);
+      const userErr = { code: 'EEXIST' };
+      throw userErr;
+    })
+    .catch((err) => {
+      logTool('Error access: ', `${path}, err.code: ${err.code}`);
+      if (err.code === 'ENOENT') return;
+      throw err;
+    });
+};
